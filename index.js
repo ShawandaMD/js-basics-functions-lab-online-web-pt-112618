db54fef7f953b79a77bcd60db8689418f56beaba
Shawanda Davis <shawandadavis93@gmail.com>
2019-07-06 16:54:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  return Math.abs(42 - location);
+}
+
+function distanceFromHqInFeet(x) {
+  return distanceFromHqInBlocks(x) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+  if (distanceTravelledInFeet(start, end) <= 400) {
+    return 0
+  }
+  else if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) < 2000) {
+    return distanceTravelledInFeet(start, end) *.02
+  }
+   
+}
