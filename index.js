0eef7e166614e7733c38104c0f8a64dfd2771813
Shawanda Davis <shawandadavis93@gmail.com>
2019-07-06 17:13:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function distanceTravelledInFeet(start, end) {
 
 function calculatesFarePrice(start, end) {
   if (distanceTravelledInFeet(start, end) <= 400) {
-    return 0
+    return 0;
   }
   else if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) < 2000) {
     return (distanceTravelledInFeet(start, end) - 400) *.02
@@ -20,6 +20,8 @@ function calculatesFarePrice(start, end) {
   else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end) <= 2500) {
     return 25
   } 
-  else
+  else {
+    return 'No can do.';
+  }
    
 }
